b3660aec731f5fb9366b342c7e2ca2d120382e99
Axel Geertinger <atge@kb.dk>
2020-08-31 17:37:03 +0200

    prepare PDF download

--- url_to_pdf.sh
+++ url_to_pdf.sh
@@ -3,8 +3,9 @@ URL=$1
 if [ -z "$URL" ]; then
   echo "No URL specified"
 else
+  rm output.pdf
   echo "Render to PDF: $URL"
   google-chrome-stable --headless --disable-gpu --print-to-pdf $URL --virtual-time-budget=10000
-  chmod 644 output.pdf
+  chmod 666 output.pdf
   ls -l output.pdf
 fi
