929a40ca0f6d196538764d56349a6cf760ebb9f2
Axel Geertinger <atge@kb.dk>
2022-05-18 23:07:18 +0200

    Update url_to_pdf.sh
    Add --run-all-compositor-stages-before-draw option to delay PDF printing

--- url_to_pdf.sh
+++ url_to_pdf.sh
@@ -6,6 +6,6 @@ then
 else
   echo "Render to PDF: $URL"
   rm output.pdf
-  google-chrome-stable --headless --disable-gpu --print-to-pdf $URL --virtual-time-budget=10000 --print-to-pdf-no-header
+  google-chrome-stable --headless --disable-gpu --print-to-pdf $URL --virtual-time-budget=10000 --run-all-compositor-stages-before-draw --print-to-pdf-no-header
   chmod 666 output.pdf
 fi
